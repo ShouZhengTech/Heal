6987e260a0078e04ad676e496be3df3de35da72a
peter <peter@peterdeiMac.local>
2017-07-12 09:39:47 +0800

    Add Why join us text

--- js/why.js
+++ js/why.js
@@ -1,18 +1,16 @@
-var tabNames = ["sick", "well", "doctor"];
-
-function selectTab(clickedTabName){
-  var tab = $('.tabs .'+clickedTabName);
-  for (var i=0;i<tabNames.length;i++) {
-    var tabName = tabNames[i];
-    $(".tabs ."+tabName).removeClass("selected");
-    $("section .container #"+tabName).addClass("hidden");
-  }
-  tab.addClass("selected");
-  $("section .container #"+clickedTabName).removeClass("hidden");
-  console.log(tab);
-}
-
 $(document).ready(function() {
+  var tabNames = ["sick", "well", "doctor"];
+
+  function selectTab(clickedTabName){
+    var tab = $('.tabs .'+clickedTabName);
+    for (var i=0;i<tabNames.length;i++) {
+      var tabName = tabNames[i];
+      $(".tabs ."+tabName).removeClass("selected");
+      $("section .container #"+tabName).addClass("hidden");
+    }
+    tab.addClass("selected");
+    $("section .container #"+clickedTabName).removeClass("hidden");
+  }
 
   $('.tabs li a').on('click',function(e){
     var tab = $(e.currentTarget);
@@ -24,13 +22,18 @@ $(document).ready(function() {
     $(this).addClass('active');
   });
 
-  var tab = String(window.location).split("#")[1];
-  if(typeof(tab) == "undefined"){
-    selectTab("sick")
+  // only run why.html
+  if($('#tabs').length > 0){
+    var tab = String(window.location).split("#")[1];
+    console.log(tab);
+    console.log(typeof(tab));
+    console.log(typeof(tab) == "undefined");
+    if(typeof(tab) == "undefined"){
+      selectTab("sick")
+    }else{
+      if(tab || $.inArray(tab,tabNames)){
+        selectTab(tab);
+      }
+    }
   }
-
-  if(tab || $.inArray(tab,tabNames)){
-    selectTab(tab);
-  }
-
 });
